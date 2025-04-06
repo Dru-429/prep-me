673ecf7e027fbfe9f7fbfcd548053cd45ac2c023
Dru-429 <ect.dhruv@gmail.com>
2025-04-07 02:43:23 +0530

    temp fixed bug

--- app/components/InterviewCard.tsx
+++ app/components/InterviewCard.tsx
@@ -12,6 +12,8 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY')
 
+    console.log("user :" , userId)
+
     return (
         <div className='card-border w-[360px] max-sm:w-full min-h-96'>
             <div className='card-interview'>
