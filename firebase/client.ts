ccfcac98b8778e4d49833372c9e5243544022d87
Dru-429 <ect.dhruv@gmail.com>
2025-04-07 10:44:39 +0530

    Added client and server req to the firebase

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase-admin/auth";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAdlRLc-5TASv-CIuT2BYCUSho9mlStBOY",
   authDomain: "prepme-80009.firebaseapp.com",
@@ -17,5 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
